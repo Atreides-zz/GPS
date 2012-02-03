a89f60f7c00d69d653db5ebb1673b02b4c16ff9e
Atreides <vanquishing@gmail.com>
2012-02-03 18:10:49 +0100

    parse a grados minutos y segundos

--- src/net/atredroid/gpsbasic/gpsbasic.java
+++ src/net/atredroid/gpsbasic/gpsbasic.java
@@ -27,30 +27,58 @@ public class gpsbasic extends Activity {
 		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
 			public void onStatusChanged(String provider, int status, Bundle extras) {
-				// called when the location provider status changes. Possible status: OUT_OF_SERVICE, TEMPORARILY_UNAVAILABLE or AVAILABLE.
+				// called when the location provider status changes.
+				//Possible status: OUT_OF_SERVICE, TEMPORARILY_UNAVAILABLE or AVAILABLE.
 			}
 			public void onProviderEnabled(String provider) {
 				// called when the location provider is enabled by the user
 			}
 			public void onProviderDisabled(String provider) {
-				// called when the location provider is disabled by the user. If it is already disabled, it's called immediately after requestLocationUpdates
+				// called when the location provider is disabled by the user.
+				//If it is already disabled, it's called immediately after requestLocationUpdates
 			}
 			
 			public void onLocationChanged(Location location) {
 				double latitude = location.getLatitude();
 				double longitude = location.getLongitude();
+				
+				
+				
 				if (location.hasAltitude()) {
 					double altitude = location.getAltitude();
-					String alt = Double.toString(altitude);
+					String alt = Location.convert(altitude, Location.FORMAT_SECONDS);
+					alt = parse_location(alt);
 					tv_altitude.setText(alt);
 				
 				}
-				String lat = Double.toString(latitude);
+				String lat = Location.convert(latitude, Location.FORMAT_SECONDS);
+				lat = parse_location(lat);
 				tv_latitude.setText(lat);
-				String longi = Double.toString(longitude);
+				String longi = Location.convert(longitude, Location.FORMAT_SECONDS);
+				longi = parse_location(longi);
 				tv_longitude.setText(longi);
 	
 			}
 		});
     }
+    
+    static String parse_location(String loc) {
+    	
+    	if (loc.contains(":")) {
+    		//Regular Expression for char :
+    		String array[] = loc.split("\\:"); 
+    		for (int i = 0; i < array.length; i++) {
+    			if (i==0)
+    				loc = array[i] + "º";
+    			else if (i==1)
+    				loc += " " + array[i] + "'";
+    			else
+    				loc += " " + array[i] + '"';
+    		}
+    	}
+    	return loc;
+    }
+    
+    
+    
 }
